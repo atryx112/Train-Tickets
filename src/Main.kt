5c2d03126f503bbfa2cb48c94a4d5b5e2e9fe49c
Levente Mozes <leventemozes@yahoo.com>
2025-10-27 14:19:52 +0000

    Adding prices test

--- src/Main.kt
+++ src/Main.kt
@@ -1,10 +1,10 @@
 fun main() {
     val initialStations = listOf(
-        Station("Manchester Piccadilly", Money.of(10.00), Money.of(50.00)),
-        Station("Birmingham New Street", Money.of(10.50), Money.of(35.00)),
-        Station("Edinburgh Waverley",    Money.of(10.00), Money.of(82.00)),
-        Station("Leeds",                 Money.of(10.00), Money.of(46.00)),
-        Station("Bristol Temple Meads",  Money.of(10.00), Money.of(42.00)),
+        Station("Manchester Piccadilly", Money.of(45.00), Money.of(50.00)),
+        Station("Birmingham New Street", Money.of(65.50), Money.of(35.00)),
+        Station("Edinburgh Waverley",    Money.of(75.00), Money.of(82.00)),
+        Station("Leeds",                 Money.of(85.00), Money.of(46.00)),
+        Station("Bristol Temple Meads",  Money.of(95.00), Money.of(42.00)),
     )
 
     val network = Network(initialStations)
