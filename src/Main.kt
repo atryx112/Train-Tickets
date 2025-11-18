bc97b35d28a75fd725e8d84382feef4196f3ce0b
Levente Mozes <leventemozes@yahoo.com>
2025-11-18 15:31:41 +0000

    Added more station and fixed de right prices.

--- src/Main.kt
+++ src/Main.kt
@@ -3,10 +3,12 @@ fun main() {
         Station("Manchester Piccadilly", Money.of(55.00), Money.of(50.00)),
         Station("Birmingham New Street", Money.of(65.50), Money.of(35.00)),
         Station("Edinburgh Waverley",    Money.of(75.00), Money.of(82.00)),
-        Station("Leeds",                 Money.of(8235.00), Money.of(46.00)),
-        Station("Bristol Temple Meads",  Money.of(915.00), Money.of(42.00)),
-        Station("London",           Money.of(1915.00), Money.of(2242.00)),
-        Station("Barcelona  City Centre", Money.of(1915.00), Money.of(2242.00)),
+        Station("Reading",               Money.of(45.00), Money.of(46.00)),
+        Station("Bristol Temple Meads",  Money.of(65.00), Money.of(42.00)),
+        Station("London",                Money.of(78.00), Money.of(29.00)),
+        Station("Coventry ",             Money.of(38.00), Money.of(56.00)),
+        Station("Milton Keyness",        Money.of(49.00), Money.of(42.00)),
+        Station("Nottingham",            Money.of(68.00), Money.of(78.00)),
 
         )
 
